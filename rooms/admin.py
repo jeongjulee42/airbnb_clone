98da4be1fdd16248bcd36765050bebbdd7c80255
donkey-hockey <ljj3342@naver.com>
2022-09-06 13:56:41 +0900

    Feat(rooms): add inlineModelAdmin, raw_id_fields
    add inlineModelAdmin in admin for register photo at rooms admin pannel
    add raw_id_fields in admin for help host easy

--- rooms/admin.py
+++ rooms/admin.py
@@ -3,23 +3,51 @@ from django.utils.html import mark_safe
 from . import models
 
 
+@admin.register(models.RoomType, models.Facility, models.Amenity, models.HouseRule)
+class ItemAdmin(admin.ModelAdmin):
+    
+    """ Item Admin Definition """
+    
+    list_display = ("name", "used_by")
+    
+    def used_by(self, obj):
+        return obj.rooms.count()
+
+@admin.register(models.Photo)
+class PhotoAdmin(admin.ModelAdmin):
+
+    """ Phot Admin Definition """
+
+    list_display = ("__str__", "get_thumbnail")
+
+    def get_thumbnail(self, obj):
+        return mark_safe(f'<img width="50px" src="{obj.file.url}" />')
+
+    get_thumbnail.short_description = "Thumbnail"
+    
+
+class PhotoInline(admin.TabularInline):
+
+    model = models.Photo
+    
+    
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
     
     """Room Admin Definition"""
+    
+    inlines = (PhotoInline,)
+    
     fieldsets = (
         (
             "Basic Info",
-            {"fields": ("name", "description", "country", "address", "price")},
+            {"fields": ("name", "description", "country", "city", "address", "price")},
         ),
         ("Times", {"fields": ("check_in", "check_out", "instant_book")}),
         ("Spaces", {"fields": ("guests", "beds", "bedrooms", "baths")}),
         (
             "More About the Space",
-            {
-                "classes": ("collapse",),
-                "fields": ("amenities", "facilities", "house_rules"),
-            },
+            {"fields": ("amenities", "facilities", "house_rules")}, 
         ),
         ("Last Details", {"fields": ("host",)}),
     )
@@ -51,6 +79,7 @@ class RoomAdmin(admin.ModelAdmin):
         "city",
         "country",
     )
+    raw_id_fields = ("host",)
     
     search_fields = ("=city", "^host__username")
     
@@ -61,26 +90,3 @@ class RoomAdmin(admin.ModelAdmin):
     
     def count_photos(self, obj):
         return obj.photos.count()
-
-
-@admin.register(models.RoomType, models.Facility, models.Amenity, models.HouseRule)
-class ItemAdmin(admin.ModelAdmin):
-    
-    """ Item Admin Definition """
-    
-    list_display = ("name", "used_by")
-    
-    def used_by(self, obj):
-        return obj.rooms.count()
-
-@admin.register(models.Photo)
-class PhotoAdmin(admin.ModelAdmin):
-
-    """ Phot Admin Definition """
-
-    list_display = ("__str__", "get_thumbnail")
-
-    def get_thumbnail(self, obj):
-        return mark_safe(f'<img width="50px" src="{obj.file.url}" />')
-
-    get_thumbnail.short_description = "Thumbnail"
