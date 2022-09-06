048f7d6a2794009465c9d88a6bf20e05182320b4
donkey-hockey <ljj3342@naver.com>
2022-09-06 11:59:03 +0900

    Feat: used_by , count_photos function Add used_by func in ItemAdmin class Add count_photos func in RoomAdmin class

--- rooms/admin.py
+++ rooms/admin.py
@@ -36,6 +36,7 @@ class RoomAdmin(admin.ModelAdmin):
         "check_out",
         "instant_book",
         "count_amenities",
+        "count_photos",
     )
     
     list_filter = (
@@ -55,6 +56,9 @@ class RoomAdmin(admin.ModelAdmin):
     
     def count_amenities(self, obj):
         return obj.amenities.count()
+    
+    def count_photos(self, obj):
+        return obj.photos.count()
 
 
 @admin.register(models.RoomType, models.Facility, models.Amenity, models.HouseRule)
@@ -62,7 +66,10 @@ class ItemAdmin(admin.ModelAdmin):
     
     """ Item Admin Definition """
     
-    pass
+    list_display = ("name", "used_by")
+    
+    def used_by(self, obj):
+        return obj.rooms.count()
 
 @admin.register(models.Photo)
 class PhotoAdmin(admin.ModelAdmin):
