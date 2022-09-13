728e8221a9907a7c2aab32f6d1443dd97f2455d6
jeongjulee <ljj3342@naver.com>
2022-09-13 10:58:40 +0900

    Feat(users): add signup save func
    add signup save func for new user save

--- users/views.py
+++ users/views.py
@@ -46,3 +46,11 @@ class SignUpView(FormView):
     form_class = forms.SignUpForm
     success_url = reverse_lazy("core:home")
 
+    def form_valid(self, form):
+        form.save()
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(self.request, username=email, password=password)
+        if user is not None:
+            login(self.request, user)
+        return super().form_valid(form)
