6717132d6e370352b44d27002e3fc887fd6a4fe3
jeongjulee <ljj3342@naver.com>
2022-09-13 10:28:13 +0900

    Feat(users): create login page
    create login page for user login

--- config/urls.py
+++ config/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("core.urls", namespace="core")),
+    path("users/", include("users.urls", namespace="users")),
     path("rooms/", include("rooms.urls", namespace="rooms")),
 ]
 
