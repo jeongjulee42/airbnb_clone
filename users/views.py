0a08fee9ea03a84b39ea39b3f71dd86a6c11bcef
jeongjulee <ljj3342@naver.com>
2022-09-16 12:00:15 +0900

    Feat(password): add password update page
    add password update page for change password

--- users/views.py
+++ users/views.py
@@ -1,7 +1,8 @@
 
 import os
 import requests
-from django.views.generic import FormView, DetailView
+from django.contrib.auth.views import PasswordChangeView
+from django.views.generic import FormView, DetailView, UpdateView
 from django.urls import reverse_lazy
 from django.shortcuts import redirect, reverse
 from django.contrib.auth import authenticate, login, logout
@@ -218,3 +219,26 @@ class UserProfileView(DetailView):
 
     model = models.User
     context_object_name = "user_obj"
+
+
+class UpdateProfileView(UpdateView):
+
+    model = models.User
+    template_name = "users/update-profile.html"
+    fields = (
+        "first_name",
+        "last_name",
+        "gender",
+        "bio",
+        "birthdate",
+        "language",
+        "currency",
+    )
+
+    def get_object(self, queryset=None):
+        return self.request.user
+    
+
+class UpdatePasswordView(PasswordChangeView):
+
+    template_name = "users/update-password.html"
