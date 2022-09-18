a74b1bdbc503c4d2e2197ddced9583ffc6118865
jeongjulee <ljj3342@naver.com>
2022-09-18 10:55:33 +0900

    Feat(rooms): add calendar for show days
    add calendar for show days

--- rooms/models.py
+++ rooms/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.urls import reverse
 from django_countries.fields import CountryField
 from core import models as core_models
+from cal import Calendar
 
 
 class AbstractItem(core_models.TimeStampedModel):
@@ -119,3 +120,7 @@ class Room(core_models.TimeStampedModel):
         photos = self.photos.all()[1:5]
         return photos
     
+    def get_calendars(self):
+        calendar = Calendar(2019, 11)
+        print(calendar.get_month())
+        return False
