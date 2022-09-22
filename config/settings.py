1ed71d5ffdf6810eabd39f3de7ee822db99dcbbb
jeongjulee <ljj3342@naver.com>
2022-09-22 14:47:57 +0900

    Feat(users): change language for users
    change language

--- config/settings.py
+++ config/settings.py
@@ -62,6 +62,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.locale.LocaleMiddleware",
 ]
 
 ROOT_URLCONF = 'config.urls'
@@ -166,3 +167,8 @@ LOGIN_URL = "/users/login/"
 # Locale
 
 LOCALE_PATHS = (os.path.join(BASE_DIR, "locale"),)
+
+# Language
+LANGUAGE_COOKIE_NAME = "django_language"
+
+LANGUAGE_CODE = "en"
--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 
 import os
 import requests
+from django.utils import translation
 from django.http import HttpResponse
 from django.contrib.auth.views import PasswordChangeView
 from django.views.generic import FormView, DetailView, UpdateView
@@ -12,7 +13,7 @@ from django.core.files.base import ContentFile
 from django.contrib import messages
 from django.contrib.messages.views import SuccessMessageMixin
 from . import forms, models, mixins
-
+from config import settings
 
 # class LoginView(View):
 #     def get(self, request):
@@ -293,5 +294,7 @@ def switch_hosting(request):
 def switch_language(request):
     lang = request.GET.get("lang", None)
     if lang is not None:
-        pass
-    return HttpResponse(status=200)
+        translation.activate(lang)
+    response = HttpResponse(200)
+    response.set_cookie(settings.LANGUAGE_COOKIE_NAME, lang)
+    return response
