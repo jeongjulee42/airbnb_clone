ee4397dfc467c3ffaf646d5269c82fb52f8ea084
jeongjulee <ljj3342@naver.com>
2022-09-23 11:53:54 +0900

    Feat: more translate
    more translate user informations, page, ...

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 
 import uuid
+from django.utils.translation import gettext_lazy as _
 from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import AbstractUser
@@ -18,15 +19,18 @@ class User(AbstractUser):
     GENDER_OTHER = "other"
     
     GENDER_CHOICES = (
-        (GENDER_MALE, "Male"),
-        (GENDER_FEMALE, "Female"),
-        (GENDER_OTHER, "Other"),
+        (GENDER_MALE, _("Male")),
+        (GENDER_FEMALE, _("Female")),
+        (GENDER_OTHER, _("Other")),
     )
     
     LANGUAGE_ENGLISH = "en"
     LANGUAGE_KOREAN = "kr"
 
-    LANGUAGE_CHOICES = ((LANGUAGE_ENGLISH, "English"), (LANGUAGE_KOREAN, "Korean"))
+    LANGUAGE_CHOICES = (
+        (LANGUAGE_ENGLISH, _("English")),
+        (LANGUAGE_KOREAN, _("Korean")),
+    )
 
     CURRENCY_USD = "usd"
     CURRENCY_KRW = "krw"
@@ -43,10 +47,18 @@ class User(AbstractUser):
         (LOGING_KAKAO, "Kakao"),
     )
     avatar = models.ImageField(upload_to="avatars", blank=True, null=True)
-    gender = models.CharField(choices=GENDER_CHOICES, max_length=10, blank=True, null=True)
-    bio = models.TextField(blank=True)
+    gender = models.CharField(
+        _("gender"), choices=GENDER_CHOICES, max_length=10, blank=True
+    )
+    bio = models.TextField(_("bio"), blank=True)
     birthdate = models.DateField(blank=True, null=True)
-    language = models.CharField(choices=LANGUAGE_CHOICES, max_length=2, blank=True, null=True)
+    language = models.CharField(
+        _("language"),
+        choices=LANGUAGE_CHOICES,
+        max_length=2,
+        blank=True,
+        default=LANGUAGE_KOREAN,
+    )
     currency = models.CharField(choices=CURRENCY_CHOICES, max_length=3, blank=True, null=True)
     superhost = models.BooleanField(default=False)
     email_verified = models.BooleanField(default=False)
@@ -66,7 +78,7 @@ class User(AbstractUser):
                 "emails/verify_email.html", {"secret": secret}
             )
             send_mail(
-                "Verify Airbnb Account",
+                _("Verify Airbnb Account"),
                 strip_tags(html_message),
                 settings.EMAIL_FROM,
                 [self.email],
